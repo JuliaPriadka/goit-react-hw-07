619dd409ef9c9551b71abbcfea5872be485823be
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-06-21 21:10:33 +0300

    Added loader & error message

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -14,34 +14,41 @@ const contactsSlice = createSlice({
     builder
       .addCase(fetchContacts.pending, state => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.loading = false;
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(addContact.pending, state => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(addContact.fulfilled, (state, action) => {
         state.loading = false;
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(deleteContact.pending, state => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
         state.items.splice(index, 1);
       })
       .addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
         state.error = action.payload;
       });
   },
